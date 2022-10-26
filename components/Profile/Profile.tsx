817533ef9f5a9afb452cfe4bb3aa60395fa0baa7
KarthikS373 <lit2021012@iiitl.ac.in>
2022-10-26 16:55:54 +0530

    Revert "Profile fetch"
    This reverts commit 8ba17d94dcbef225ce49b8baeae08610dbba773e.

--- components/Profile/Profile.tsx
+++ components/Profile/Profile.tsx
@@ -1,3 +1,8 @@
+// Firstly I need access to some things... (I did ask Abhinay, he said he dont have those)
+// 1. Access to Database and access tokens for same
+// 2. API endpoints or API docs of staking.metabillionaire.com/ as the requests are of cross origin I also need them to enable CORs requests
+// 3. Does the user twitter profile or any URI connected to the wallet or anywhere in the profile (coz I couldnt find them over the current user schema. if yes I need access to those else do u want me to create a schema or a seperate form where user can actually create one??)
+
 import React from "react";
 import { BsTwitter } from "react-icons/bs";
 import { FaDiscord } from "react-icons/fa";
@@ -21,13 +26,8 @@ import {
   Tab,
   TabPanels,
   TabPanel,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionIcon,
-  AccordionPanel,
 } from "@chakra-ui/react";
-import NFTCard from "./NFTCard";
+import { MdAttachMoney } from "react-icons/md";
 
 interface StatData {
   label: string;
@@ -37,21 +37,21 @@ interface StatData {
 const statData: StatData[] = [
   {
     label: "Current Holdings",
-    score: "999 MBUC",
+    score: "3.2M",
   },
   {
     label: "Staked",
-    score: "38",
+    score: "77k",
   },
   {
-    label: "Raffle Entered",
-    score: "18",
+    label: "Raffle Entried",
+    score: "2544",
   },
 ];
 
 const UserProfile = ({
   username = "cyberpunk373",
-  userId = "userId",
+  nftData = [],
   products = [],
 }) => {
   const TABS = [
@@ -59,20 +59,17 @@ const UserProfile = ({
       id: 1,
       title: "NFTs",
       content: (
-        <SimpleGrid columns={[1, null, 3]} spacing={10}>
-          {products.length != 0 ? (
-            products.map((nft: any) => {
-              return (
-                <NFTCard
-                  key={nft.slug}
-                  nftName={nft.name}
-                  nftImage={nft.image_url}
-                />
-              );
-            })
-          ) : (
-            <NFTCard />
-          )}
+        <SimpleGrid>
+          {/* {nftData
+            ? nftData.map((nft: any) => {
+                return <>{nft.name}</>;
+              })
+            : "Nothing to display"} */}
+          {products.length != 0
+            ? products.map((nft: any) => {
+                return <>{nft.name}</>;
+              })
+            : "Nothing to display"}
         </SimpleGrid>
       ),
     },
@@ -95,7 +92,6 @@ const UserProfile = ({
 
   const [isLessThan550] = useMediaQuery("(max-width: 550px)");
   const [isLessThan360] = useMediaQuery("(max-width: 360px)");
-  const [isLessThan768] = useMediaQuery("(max-width: 768px)");
 
   return (
     <Flex
@@ -106,133 +102,115 @@ const UserProfile = ({
       alignItems={"stretch"}
       m="auto"
     >
-      <Flex flexDir={"column"} gap={"md"} m="auto">
-        <Box
-          maxW={"320px"}
-          minW={isLessThan360 ? "160px" : "320px"}
-          alignItems={"flex-start"}
-          maxH={"min-content"}
-          m="auto"
+      <Box
+        maxW={"320px"}
+        minW={isLessThan360 ? "160px" : "320px"}
+        alignItems={"flex-start"}
+        maxH={"min-content"}
+        m="auto"
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        overflow={"hidden"}
+        flex={1}
+      >
+        <Image
+          h={"120px"}
           w={"full"}
-          bg={useColorModeValue("white", "gray.800")}
-          boxShadow={"2xl"}
-          rounded={"md"}
-          overflow={"hidden"}
-          flex={1}
-        >
-          <Image
-            h={"120px"}
-            w={"full"}
+          src={
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgThQ2FqJAPuOzIQnx5rzOskzlldJWcA_TTQ&usqp=CAU"
+          }
+          alt="Abstract background"
+          objectFit={"cover"}
+        />
+        <Flex justify={"center"} mt={-12}>
+          <Avatar
+            size={"xl"}
             src={
-              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgThQ2FqJAPuOzIQnx5rzOskzlldJWcA_TTQ&usqp=CAU"
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ2jOmIfUnjeE35drnm9qtmql4vDCdphjXEL1FG5TO_yTb-cRdpA1qmgzqO6CtCEjV53c&usqp=CAU"
             }
-            alt="Abstract background"
-            objectFit={"cover"}
+            css={{
+              border: "5px solid white",
+            }}
           />
-          <Flex justify={"center"} mt={-12}>
-            <Avatar
-              size={"xl"}
-              src={
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSQ2jOmIfUnjeE35drnm9qtmql4vDCdphjXEL1FG5TO_yTb-cRdpA1qmgzqO6CtCEjV53c&usqp=CAU"
-              }
-              css={{
-                border: "5px solid white",
-              }}
-            />
-          </Flex>
+        </Flex>
 
-          <Box p={6}>
-            <VStack textAlign={"center"} w="min-content" m="auto">
-              <Heading fontSize={"sm"} whiteSpace="nowrap">
-                {username}
-              </Heading>
-              <Text color={"gray.500"} fontSize="x-small">
-                {userId.slice(0, 3)}....{userId.slice(-5, -1)}
-              </Text>
-            </VStack>
-            <Text
-              pt="16px"
-              fontWeight="light"
-              fontFamily="sans-serif"
-              textAlign="justify"
-            >
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-              ipsa. Repudiandae similique numquam saepe aliquam provident veniam
-              eveniet, quis cupiditate vitae minima unde nobis. Ut sit totam,
-              vitae saepe voluptas voluptates unde.
+        <Box p={6}>
+          <VStack textAlign={"center"} w="min-content" m="auto">
+            <Heading fontSize={"sm"} whiteSpace="nowrap">
+              {username}
+            </Heading>
+            <Text color={"gray.500"} fontSize="x-small">
+              Account number
             </Text>
+          </VStack>
+          <Text
+            pt="16px"
+            fontWeight="light"
+            fontFamily="sans-serif"
+            textAlign="justify"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
+            ipsa. Repudiandae similique numquam saepe aliquam provident veniam
+            eveniet, quis cupiditate vitae minima unde nobis. Ut sit totam,
+            vitae saepe voluptas voluptates unde.
+          </Text>
 
-            <VStack mt={8} direction={"row"} spacing={4}>
-              <Button
-                flex={1}
-                py={1.5}
-                px={14}
-                fontSize={"sm"}
-                rounded={"full"}
-                bg={"blue.400"}
-                color={"white"}
-                boxShadow={
-                  "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
-                }
-                _hover={{
-                  bg: "blue.500",
-                }}
-                _focus={{
-                  bg: "blue.500",
-                }}
+          <VStack mt={8} direction={"row"} spacing={4}>
+            <Button
+              flex={1}
+              py={1.5}
+              px={14}
+              fontSize={"sm"}
+              rounded={"full"}
+              bg={"blue.400"}
+              color={"white"}
+              boxShadow={
+                "0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)"
+              }
+              _hover={{
+                bg: "blue.500",
+              }}
+              _focus={{
+                bg: "blue.500",
+              }}
+            >
+              <Icon as={BsTwitter} boxSize={10} />
+              &nbsp;
+              <Text
+                fontFamily={"sans-serif"}
+                fontWeight="200"
+                fontSize={"22"}
+                pt="1"
               >
-                <Icon as={BsTwitter} boxSize={10} />
-                &nbsp;
-                <Text
-                  fontFamily={"sans-serif"}
-                  fontWeight="200"
-                  fontSize={"22"}
-                  pt="1"
-                >
-                  Twitter
-                </Text>
-              </Button>
-              <Button
-                flex={1}
-                py={1.5}
-                px={14}
-                fontSize={"md"}
-                rounded={"full"}
-                _focus={{
-                  bg: "gray.200",
-                }}
+                Twitter
+              </Text>
+            </Button>
+            <Button
+              flex={1}
+              py={1.5}
+              px={14}
+              fontSize={"md"}
+              rounded={"full"}
+              _focus={{
+                bg: "gray.200",
+              }}
+            >
+              <Icon as={FaDiscord} boxSize={10} />
+              &nbsp;
+              <Text
+                fontFamily={"sans-serif"}
+                fontWeight="200"
+                fontSize={"22"}
+                pt="1"
               >
-                <Icon as={FaDiscord} boxSize={10} />
-                &nbsp;
-                <Text
-                  fontFamily={"sans-serif"}
-                  fontWeight="200"
-                  fontSize={"22"}
-                  pt="1"
-                >
-                  Discord
-                </Text>
-              </Button>
-            </VStack>
-          </Box>
+                Discord
+              </Text>
+            </Button>
+          </VStack>
         </Box>
-        {/* <Button
-          fontSize={[12, 12, 15, 15]}
-          borderRadius="full"
-          colorScheme="customBlue"
-          _hover={{
-            color: "white",
-            bgColor: "customBlue.500",
-            borderColor: "customBlue.500",
-          }}
-          // w={[150, 150, 200, 200]}
-          shadow="md"
-          fontWeight={400}
-          ml={["xs", "xs", "sm", "md"]}
-        >
-          CLAIM MBUC
-        </Button> */}
-      </Flex>
+      </Box>
       <Box
         flex={3}
         textAlign={"justify"}
@@ -240,7 +218,7 @@ const UserProfile = ({
       >
         <Stack minW={"320px"} m="auto">
           <SimpleGrid
-            columns={isLessThan768 ? 2 : 4}
+            columns={isLessThan550 ? 2 : 4}
             spacing={5}
             pt={4}
             pl={{ base: 0, md: 10 }}
@@ -253,80 +231,72 @@ const UserProfile = ({
                 py={1}
                 pr={1}
                 borderLeft="2px solid"
-                borderLeftColor="customBlue.500"
+                borderLeftColor="blue.400"
                 justifyContent="space-between"
               >
                 <Box
                   fontSize="md"
                   fontWeight="bold"
-                  color="customBlue.500"
+                  color="blue.400"
                   fontFamily={"sans-serif"}
                 >
                   {data.score}
                 </Box>
                 <Text
                   whiteSpace={"nowrap"}
-                  fontSize={{ base: "16px", md: "40px", lg: "md" }}
+                  fontSize="sm"
                   fontFamily={"sans-serif"}
                 >
                   {data.label}
                 </Text>
               </Stack>
             ))}
+            <Button
+              fontSize={[12, 12, 15, 15]}
+              px="sm"
+              borderRadius="full"
+              colorScheme="customBlue"
+              _hover={{
+                color: "white",
+                bgColor: "customBlue.500",
+                borderColor: "customBlue.500",
+              }}
+              w={[150, 150, 200, 200]}
+              shadow="md"
+              fontWeight={400}
+              ml={["xs", "xs", "sm", "md"]}
+            >
+              CLAIM MBUC
+            </Button>
           </SimpleGrid>
         </Stack>
-        {isLessThan768 ? (
-          <Accordion mt={"8"} variant="enclosed" defaultIndex={0}>
-            {TABS.map((tab) => {
-              return (
-                <AccordionItem key={tab.id}>
-                  <AccordionButton>
-                    <Box flex="1" textAlign="left">
-                      <Text
-                        fontFamily={"sans-serif"}
-                        fontSize="md"
-                        color={"customBlue.500"}
-                        fontWeight={"hairline"}
-                      >
-                        {tab.title}
-                      </Text>
-                    </Box>
-                    <AccordionIcon />
-                  </AccordionButton>
-                  <AccordionPanel pb={4}>{tab.content}</AccordionPanel>
-                </AccordionItem>
-              );
-            })}
-          </Accordion>
-        ) : (
-          <Tabs
-            mt={"8"}
-            isFitted
-            isLazy
-            variant="enclosed"
-            defaultIndex={0}
-            w={isLessThan550 ? "95vw" : "70vw"}
-          >
-            <TabList>
-              {TABS.map((tab) => (
-                <Tab key={tab.id}>
-                  <Text
-                    fontFamily={"sans-serif"}
-                    fontSize="md"
-                    fontWeight={"hairline"}
-                  >
-                    {tab.title}
-                  </Text>
-                </Tab>
-              ))}
-            </TabList>
-            <TabPanels>
-              {TABS.map((tab) => (
-                <TabPanel key={tab.id}>{tab.content}</TabPanel>
-              ))}
-            </TabPanels>
-          </Tabs>
-        )}
+        <Tabs
+          mt={"8"}
+          isFitted
+          isLazy
+          variant="enclosed"
+          defaultIndex={0}
+          w={isLessThan550 ? "95vw" : "70vw"}
+        >
+          <TabList>
+            {TABS.map((tab) => (
+              <Tab key={tab.id}>
+                <Text
+                  fontFamily={"sans-serif"}
+                  fontSize="md"
+                  fontWeight={"hairline"}
+                >
+                  {tab.title}
+                </Text>
+              </Tab>
+            ))}
+          </TabList>
+          <TabPanels>
+            {TABS.map((tab) => (
+              <TabPanel key={tab.id}>{tab.content}</TabPanel>
+            ))}
+          </TabPanels>
+        </Tabs>
         <Box w="100%"></Box>
       </Box>
     </Flex>
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -7,37 +7,73 @@ import UserProfile from "../components/Profile/Profile";
 import api from "../lib/api";
 import contractABI from "../artifacts/contracts/MarketplaceERC20.sol/Marketplace.json";
 
+import useEthersProvider from "../hooks/useEthersProvider";
+
 const Profile = () => {
-  const [userProducts, setUserProducts] = useState([]);
   // 0x4eb1c0bdb7f6bec8346db6593b32ef588fda5bff -> MBUC 1
   // 0x5828c6fA3F0432531BbC79f683272eA1F44488e4 --> NFT
+  const [userProducts, setUserProducts] = useState([]);
+
+  const { provider } = useEthersProvider();
+  const contract = new ethers.Contract(
+    "0x5828c6fA3F0432531BbC79f683272eA1F44488e4",
+    contractABI.abi,
+    provider
+  );
+
+  const getUserOwnedProducts = async () => {
+    const provider = new ethers.providers.JsonRpcProvider(
+      process.env.NEXT_PUBLIC_POLYGON_API_KEY
+    );
+
+    const contract = new ethers.Contract(
+      process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT!,
+      contractABI.abi,
+      provider
+    );
+    try {
+      const products = await contract.getUserOwnedProducts(
+        "0xc8C247c212722A02170970dB97226dE6bbd37c7D"
+      );
+      console.log(products.toString());
+      setUserProducts(products);
+    } catch (err) {
+      console.log("error when fetching store content on contract");
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getUserOwnedProducts();
+  });
+
+  const [nftData, setNftData] = useState([]);
   // const address = "0xc8C247c212722A02170970dB97226dE6bbd37c7D";
   // const address = "0xeec2c5a427d38cec72fe2f1cf64b42dd7de60961";
-  // const address = "0x4b120516ec475d651b759bb9d7ee4fb7d2b811a4"; -> owns mb nft
-  // const address = "0x7d6475d5961e894112737309a93dc2e3ab96beab"; -> owns mb nft and otehrs
-
-  const address = "0x7d6475d5961e894112737309a93dc2e3ab96beab";
-  useEffect(() => {
-    const fetchUserData = async () => {
-      const collectionData = await axios.get(
-        `https://api.opensea.io/api/v1/collections?format=json&limit=300&asset_owner=${address}`
-      );
-      console.log(collectionData);
-      const filteredData = collectionData.data.filter((nft: any) => {
-        return (
-          nft.name.toUpperCase().includes("METABILLIONAIRE") ||
-          nft.description.toUpperCase().includes("METABILLIONAIRE")
-        );
-      });
-      setUserProducts(filteredData);
-    };
-
-    fetchUserData();
-  }, []);
+  // useEffect(() => {
+  //   (async () => {
+  //     const collectionData = await axios.get(
+  //       `https://api.opensea.io/api/v1/collections?format=json&limit=300&asset_owner=${address}`,
+  //       {
+  //         headers: {
+  //           //@ts-ignore
+  //           //! "X-API-KEY": process.env.OPENSEA_KEY,
+  //         },
+  //       }
+  //     );
+  //     console.log(collectionData);
+  //     return collectionData;
+  //   })()
+  //     // @ts-ignore
+  //     .then((_collectionData) => {
+  //       setNftData(_collectionData.data);
+  //     })
+  //     .catch((err) => console.log(err));
+  // }, []);
 
   return (
     <Layout>
-      <UserProfile userId={address} products={userProducts} />
+      <UserProfile nftData={nftData} products={userProducts} />
     </Layout>
   );
 };
