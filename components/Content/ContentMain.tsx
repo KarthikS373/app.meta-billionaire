40b042881526fd1b1c8b27c31b126bfa88b3c0f2
KarthikS373 <lit2021012@iiitl.ac.in>
2023-03-07 00:28:50 +0530

    UI fixes content

--- components/Content/ContentMain.tsx
+++ components/Content/ContentMain.tsx
@@ -2,70 +2,15 @@ import React from "react";
 import { Badge, Box, Flex, Slider, Text } from "@chakra-ui/react";
 import Image from "next/image";
 
-import CourseCard from "./CourseCard";
-import ContentCard from "./ContentCard";
+import courses from "../../data/courses.json";
+
 import Row from "./Rows";
 
-const temp = [
+const podcast = [
   {
     id: 0,
-    name: "Course",
-    banner:
-      "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolorem.",
-  },
-  {
-    id: 1,
-    name: "Course",
-    banner:
-      "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolorem.",
-  },
-  {
-    id: 2,
-    name: "Course",
-    banner:
-      "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolorem.",
-  },
-  {
-    id: 3,
-    name: "Course",
-    banner:
-      "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolorem.",
-  },
-  {
-    id: 4,
-    name: "Course",
-    banner:
-      "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolorem.",
-  },
-  {
-    id: 5,
-    name: "Course",
-    banner:
-      "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolorem.",
-  },
-  {
-    id: 6,
-    name: "Course",
-    banner:
-      "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, dolorem.",
-  },
-  {
-    id: 7,
-    name: "Course",
+    name: "Podcast name",
+    duration: "2.45hr",
     banner:
       "https://images.unsplash.com/photo-1572177812156-58036aae439c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cHJvamVjdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     description:
@@ -76,75 +21,10 @@ const temp = [
 const ContentMain = () => {
   return (
     <>
-      <Box w={"full"}>
-        <Row data={temp} title={"Courses"} />
-        {/* <Box fontSize="14px" px={[null, "10px", "5%", "10%"]} m="10px 0 50px 0">
-          <Box
-            textAlign="center"
-            className="center flex-col"
-            p="10px"
-            mb="20px"
-          >
-            <Text
-              fontSize="20px"
-              fontWeight="700"
-              color="brand.900"
-              lineHeight="40px"
-              my="10px"
-            >
-              {"Courses"}
-            </Text>
-            <Text className="md:w-2/3 font-sans text-center">
-              {
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, repudiandae iste tenetur nisi, hic magni unde dolorem eos odit, enim excepturi nam iusto!"
-              }
-            </Text>
-          </Box>
-          <Flex
-            justify="space-between"
-            flexWrap="wrap"
-            px={["20px", "20px", null]}
-          >
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
-          </Flex>
-        </Box>
-        <Box fontSize="14px" px={[null, "10px", "5%", "10%"]} m="10px 0 50px 0">
-          <Box
-            textAlign="center"
-            className="center flex-col"
-            p="10px"
-            mb="20px"
-          >
-            <Text
-              fontSize="20px"
-              fontWeight="700"
-              color="brand.900"
-              lineHeight="40px"
-              my="10px"
-            >
-              {"Podcast"}
-            </Text>
-            <Text className="md:w-2/3 font-sans text-center">
-              {
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, repudiandae iste tenetur nisi, hic magni unde dolorem eos odit, enim excepturi nam iusto!"
-              }
-            </Text>
-          </Box>
-          <Flex
-            className="center flex-col"
-            px={["20px", "20px", null]}
-          >
-            <ContentCard />
-            <ContentCard />
-            <ContentCard />
-            <ContentCard />
-          </Flex>
-        </Box> */}
+      <Box w={"full"} className="pt-4">
+        <Row data={courses.categories} popupText="Number of lessons" title={"Courses"} />
+        <Row data={podcast} title={"Podcast"} />
+        {/* <Row data={temp} title={"AMA"} /> */}
       </Box>
     </>
   );
--- components/Content/Rows.tsx
+++ components/Content/Rows.tsx
@@ -5,6 +5,7 @@ import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 interface Props {
   data: Data[] | any[];
   title: string;
+  popupText?: string;
 }
 
 interface Data {
@@ -13,7 +14,7 @@ interface Data {
   banner: string;
 }
 
-const Row = ({ title, data }: Props) => {
+const Row = ({ title, data, popupText = "Duration" }: Props) => {
   const rowRef = useRef<HTMLDivElement>(null);
   const [isMoved, setIsMoved] = useState(false);
 
@@ -32,14 +33,17 @@ const Row = ({ title, data }: Props) => {
   };
 
   return (
-    <Box className="h-40 space-y-0.5 md:space-y-2 ml-0 mt-2 md:mt-8 translate-x-4 md:translate-x-8">
-      <Heading className="cursor-pointer w-min text-sm font-semibold text-[#294BF5]/50 transition-colors duration-200 hover:text-[#294BF5] md:text-2xl uppercase whitespace-nowrap">
+    <Box className="space-y-1 ml-0 mt-1 md:mt-2 translate-x-2 md:translate-x-8">
+      <Heading
+        fontSize={[20, 20, 26, 36]}
+        className="cursor-pointer w-min text-sm font-semibold text-black/50 transition-colors duration-200 hover:text-black md:text-2xl uppercase whitespace-nowrap"
+      >
         {title}
       </Heading>
       <Box className="relative">
         <Box
           h="full"
-          className="group z-50 hover:bg-black/30 absolute top-0 bottom-0 left-2 center m-auto"
+          className="hidden md:flex group z-50 hover:bg-black/30 absolute top-0 bottom-0 left-2 center m-auto"
         >
           <BsChevronLeft
             className={`m-auto h-9 w-9 cursor-pointer opacity-0 transition group-hover:text-white hover:scale-125 group-hover:opacity-100 ${
@@ -56,15 +60,16 @@ const Row = ({ title, data }: Props) => {
               width: "0",
             },
           }}
-          className="flex items-end justify-center scrollbar-hide space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
+          className="flex items-end justify-start scrollbar-hide space-x-1 overflow-x-scroll md:space-x-2.5 md:p-2 last:mr-8"
         >
           {data.map((outline) => (
             <Box
               key={outline.id}
-              className="group relative rounded-sm md:rounded h-28 min-w-[180px] cursor-pointer overflow-hidden transition-transform duration-200 ease-out md:h-52 md:min-w-[320px] md:hover:scale-105"
+              className="group relative rounded-lg md:rounded min-w-[200px] cursor-pointer overflow-hidden transition-transform duration-200 ease-out md:h-52 md:min-w-[365px] md:hover:scale-105"
             >
               <Image
                 src={outline.banner}
+                h={[64, 64, "auto", "auto"]}
                 className="rounded-sm object-cover w-full md:rounded"
                 alt={outline.title}
               />
@@ -73,14 +78,14 @@ const Row = ({ title, data }: Props) => {
                   fontSize={[16, 16, 20, 32]}
                   className="text-xl text-white/75"
                 >
-                  Courses
+                  {outline.name}
                 </Heading>
                 <Text
                   as="p"
                   fontSize={[12, 12, 14, 14]}
                   className="text-white/75"
                 >
-                  Duration: 54 hrs
+                  {popupText}: {outline.duration}
                 </Text>
               </Box>
             </Box>
@@ -89,7 +94,7 @@ const Row = ({ title, data }: Props) => {
 
         <Box
           h="full"
-          className="group z-50 hover:bg-black/30 absolute top-0 bottom-3 right-10 center m-auto cursor-pointer "
+          className="hidden md:flex group z-50 hover:bg-black/30 absolute top-0 bottom-3 right-10 center m-auto cursor-pointer "
           onClick={() => handleClick("right")}
         >
           <BsChevronRight className=" h-9 w-9 opacity-0 transition group-hover:text-white hover:scale-125 group-hover:opacity-100" />
